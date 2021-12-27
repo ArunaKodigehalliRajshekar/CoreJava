5cffecb0ea9bf12790292d5c70a256ab63e098fb
ArunaKodigehalliRajashekar <arunakr.xworkz@gmail.com>
2021-12-27 10:52:09 +0530

    inheritance

---
+++ inheritance/src/com/xworkz/inheritance/employeeDTO/Developers.java
@@ -0,0 +1,30 @@
+package com.xworkz.inheritance.employeeDTO;
+
+public class Developers extends Employee{
+	public String type="lazy";
+	public String work="never get tired";
+	public String worktype="machines";
+	public String seekingProperties="seekingthings";
+	public int minsalary=28000;
+	
+	public String coding(String coding) {
+		return coding;
+	}
+	
+	public String crazy(String crazy) {
+		return crazy;
+	}
+	
+	public String doinginovations(String innovations) {
+		return innovations;
+	}
+	
+	public int maxsalary(int maxsalary) {
+		return maxsalary;
+	}
+	
+	public boolean jobtype(boolean jobtype) {
+		return jobtype;
+	}
+
+}
